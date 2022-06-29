20eedf7de49455c2feba6acbdd71e46c072966f4
Spud97 <luca.dercole97@gmail.com>
2022-06-29 16:12:16 +0200

    ticket

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -6,7 +6,7 @@
         <li v-for="movie in moviesList" :key="movie.id">
           <span class="px-3">Titolo: {{ movie.title }}</span>
           <span class="px-3">Titolo originale: {{ movie.original_title }}</span>
-          <span class="px-3">Lingua: {{ movie.original_language }}</span>
+          <span class="px-3">Lingua: <span class="fi" :class="'fi-' + countryFlag()"></span></span>
           <span class="px-3">Voto: {{ movie.vote_average }}</span>
         </li>
       </ul>
@@ -17,7 +17,7 @@
         <li v-for="serie in seriesList" :key="serie.id">
           <span class="px-3">Titolo: {{ serie.name }}</span>
           <span class="px-3">Titolo originale: {{ serie.original_name }}</span>
-          <span class="px-3">Lingua: {{ serie.original_language }}</span>
+          <span class="px-3">Lingua: <span class="fi" :class="'fi-' + countryFlag()"></span></span>
           <span class="px-3">Voto: {{ serie.vote_average }}</span>
         </li>
       </ul>
@@ -35,6 +35,13 @@ export default {
     seriesList() {
       return state.seriesList;
     },
+    countryFlag() {
+      if (this.movie) {
+        return this.movie.original_language;
+      } else if (this.serie) {
+        return this.serie.original_language;
+      }
+    }
   },
 };
 </script>
